bc4ee8e02957bf5827abcd609651cbfd48a89167
flynth <flynth.dev@gmail.com>
2022-12-05 18:46:14 -0400

    Create  Middleware Authentication

---
+++ API/middleware/Authentication.ts
@@ -0,0 +1,24 @@
+import type { response } from "../utils/types/response.ts"
+import { DJWT } from "../service/JsonWebToken/main.ts"
+import { UserDB } from "../db/user/main.ts"
+
+
+export async function Authenticate(jwt:string):Promise<response>{
+    
+    const verify= await DJWT.Verify(jwt)
+    if( !verify.ok ){ return verify }        
+    
+    if( verify.body === undefined ){ 
+       return {body:verify,message:"undefined body",ok:false}  
+    }
+
+    const resp = await UserDB.accounts.findOne(verify.body)
+
+    if( resp === undefined ){ 
+        return {body:verify,message:"no exist",ok:false}  
+    }
+
+    return {body:{resp},message:"OK",ok:true}
+
+}
+
