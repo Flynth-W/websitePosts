8f224c067c9481c15419186eaf2ce45a7968a92d
flynth <flynth.dev@gmail.com>
2022-12-13 18:10:16 -0400

    Delete  _profiles() use _profile()

--- API/routes/main.ts
+++ API/routes/main.ts
@@ -2,7 +2,6 @@ import { _route } from "../utils/routes.ts"
 
 import { _account  }  from "./account/main.ts"
 import { _profile  }  from "./profile/main.ts"
-import { _profiles }  from "./profiles/main.ts"
 import { _posts    }  from "./posts/main.ts"
 import { _comments }  from "./comments/main.ts"
 import { _savePost }  from "./savePost/main.ts"
@@ -13,7 +12,6 @@ export function RouteHandler(req:Request){
   switch (routes[0]){
     case "account"  : return _account(req)
     case "profile"  : return _profile(req)
-    case "profiles" : return _profiles(req)
     case "posts"    : return _posts(req)
     case "comments" : return _comments(req)
     case "savePost" : return _savePost(req)
