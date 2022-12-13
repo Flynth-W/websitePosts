fc42d5730c381370231a8275e00ec5204f774ffb
flynth <flynth.dev@gmail.com>
2022-12-13 19:08:32 -0400

    Include fn  Promise to account model

---
+++ API/model/user/checks/account/name.ts
@@ -0,0 +1,16 @@
+import { UserDB } from "../../../../db/user/main.ts"
+import { ModelChecks } from "../../../../deps.ts"
+import type { response } from "../../../../deps.ts"
+const length =  ModelChecks.string.length(1,15)
+
+export async function _name(name:string):Promise<response>{
+  console.log("name fn")
+  const resp = length(name)
+  if(! resp.ok ){ return resp }
+
+  const only = await UserDB.accounts.findOne({name})
+  if(only === undefined ){ return {body: name ,message:"OK",ok:true} }
+  return {body: null ,message:"exist account",ok:false}
+}
+
+//console.log(await _NameAccount("juan"))
