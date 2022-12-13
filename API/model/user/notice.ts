2d8440d42ff404ac8f3114d7be58f8a762811325
flynth <flynth.dev@gmail.com>
2022-12-13 18:26:28 -0400

    Include type notice

---
+++ API/model/user/notice.ts
@@ -0,0 +1,13 @@
+import { Model  , ModelChecks } from "../../deps.ts"
+import type { property  } from "../../deps.ts"
+
+export type TypeUserNotice = {
+  nick_id   ?: property | number    // default API
+  author    ?: property | number    // default API
+  text      ?: property | string    // default API
+  link      ?: property | string    // default API
+}
+
+
+
+
