a4032fe04afdacf54465a264a35e530dd72ba5e9
flynth <flynth.dev@gmail.com>
2022-12-14 11:57:17 -0400

    Ready http://api/account LOGIN

--- API/controller/user/account/login.ts
+++ API/controller/user/account/login.ts
@@ -1,20 +1,20 @@
 import { UserDB } from "../../../db/user/main.ts"
 import { DJWT } from "../../../service/JsonWebToken/main.ts"
+import { headCore } from "../../../service/headCore/main.ts"
 
 export async function _login(req:Request){
   try{
-    // autheticate account
+    // exist account
     const _account = await req.json()
     const _resp =  await UserDB.accounts.findOne(_account)
-    if( _resp == undefined ){ console.log("no account") }
+    if( _resp == undefined ){ return new Response (JSON.stringify({message:"account not exist"}) , { status:404 , headers:headCore(req.headers) }) }
+    const  nick_id = _resp.nick_id  
 
     // return jwt 
-    const  nick_id = _resp.nick_id 
     const jwt = await DJWT.Create({nick_id })
-    if ( ! jwt.ok ){ console.log(" no jwt") }
-    console.log(jwt.body)
-    return new Response(JSON.stringify({jwt:jwt.body}))
+    if ( ! jwt.ok ){ return new Response (null , { status:500 , headers:headCore(req.headers) }) }
+    return new Response(JSON.stringify({jwt:jwt.body}) , { status:200 , headers:headCore(req.headers) })
   }catch(_e){
-    return new Response()
+    return new Response(JSON.stringify({message:_e}), { status:404 , headers:headCore(req.headers) })
   }
 }
