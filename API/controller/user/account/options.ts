204c8c60a0b7c13d2cba9fe9d64642b63818e3a5
flynth <flynth.dev@gmail.com>
2022-12-14 13:01:24 -0400

    Include headerCore

--- API/controller/user/account/options.ts
+++ API/controller/user/account/options.ts
@@ -1,5 +1,6 @@
+import { headCore } from "../../../service/headCore/main.ts"
 
-export async function _options(req:Request){
-  console.log(" this is controller /account => options")
-  return new Response()
+export function _options(req:Request){
+  console.log( " this si OPTIONS" )
+  return new Response(null,{status:202 ,headers:headCore(req.headers )})
 }
