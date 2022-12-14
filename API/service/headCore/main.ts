c4f8cae2865a5c89b4b2493d57247479d16ef9f9
flynth <flynth.dev@gmail.com>
2022-12-14 12:14:32 -0400

    Include Acces method

--- API/service/headCore/main.ts
+++ API/service/headCore/main.ts
@@ -1,12 +1,14 @@
 export function headCore(head:Headers):Headers{
   const access = {
     headers : head.get("access-control-request-headers") || "" ,
-    origin : head.get("origin") || ""
+    origin : head.get("origin") || "" ,
+    method : head.get("access-control-request-method") || ""
   }
 
   const headers= new Headers()
   headers.append("Access-Control-Allow-Origin"  , access.origin )
   headers.append("Access-Control-Allow-Headers" , access.headers )
+  headers.append("Access-Control-Allow-Methods" , access.method)
   return headers
 }
 
