98caa65bf616d5a14c09b830487e3b513ddaf2cb
flynth <flynth.dev@gmail.com>
2022-12-15 11:13:29 -0400

    TEST: ready http://api/profile GET

--- API/test/URL/profile/get/all.ts
+++ API/test/URL/profile/get/all.ts
@@ -1,6 +1,6 @@
 const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijp7Im5pY2tfaWQiOjJ9fQ.wtsw-IWT8z8gVC4KsWg0KHc7Sv4GW5ozEj8my9rIOlA"
 
-const resp = await fetch("http://localhost:8000/profile/",{
+const resp = await fetch("http://localhost:8000/profile/all",{
   method:"GET",
   headers:{
     'jwt':jwt
