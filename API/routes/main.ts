02aa533504f77a45406f27285bc4044e8a77e37a
flynth <flynth.dev@gmail.com>
2022-12-15 12:00:39 -0400

    Modify: _posts to post

--- API/routes/main.ts
+++ API/routes/main.ts
@@ -2,7 +2,7 @@ import { _route } from "../utils/routes.ts"
 
 import { _account  }  from "./account/main.ts"
 import { _profile  }  from "./profile/main.ts"
-import { _posts    }  from "./posts/main.ts"
+import { _post    }  from "./post/main.ts"
 import { _comments }  from "./comments/main.ts"
 import { _savePost }  from "./savePost/main.ts"
 import { _notice   }  from "./notice/main.ts"
@@ -13,7 +13,7 @@ export function RouteHandler(req:Request){
   switch (routes[0]){
     case "account"  : return _account(req)
     case "profile"  : return _profile(req)
-    case "posts"    : return _posts(req)
+    case "post"    : return _post(req)
     case "comments" : return _comments(req)
     case "savePost" : return _savePost(req)
     case "notice"   : return _notice(req)
