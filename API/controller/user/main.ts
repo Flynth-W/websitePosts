6d69014f8ed0f2c0bd31c0055a3199c3165107b9
flynth <flynth.dev@gmail.com>
2022-12-15 11:43:29 -0400

    Modify : posts to POST

--- API/controller/user/main.ts
+++ API/controller/user/main.ts
@@ -1,7 +1,7 @@
 import { _account } from "./account/main.ts"
 import { _comments } from "./comments/main.ts"
 import { _notice } from "./notice/main.ts"
-import { _posts }  from "./posts/main.ts"
+import { _POST }  from "./post/main.ts"
 import { _profile } from "./profile/main.ts"
 import { _savePost } from "./savePost/main.ts"
 import { _setting } from "./setting/main.ts"
@@ -10,7 +10,7 @@ export const UserCtrl = {
   account:_account ,
   comments:_comments,
   notice:_notice,
-  posts:_posts,
+  POST:_POST,
   profile:_profile,
   savePost:_savePost,
   setting:_setting
