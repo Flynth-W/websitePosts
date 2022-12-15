a51d71a107fc9f8ea26990c05653c1dae2b6ce42
flynth <flynth.dev@gmail.com>
2022-12-14 23:28:12 -0400

    Ready controll user/profile ( GET, PUT )

--- API/controller/user/profile/get.ts
+++ API/controller/user/profile/get.ts
@@ -1,5 +1,23 @@
+import { headCore } from "../../../service/headCore/main.ts"
+import { Authenticate } from "../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../db/user/main.ts"
+
 export async function _get(req:Request){
-  console.log(" this is controller /account => post")
-  return new Response()
+  console.log(" setting GET")
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers) }) } 
+
+  // get mongo
+  try{
+    const nick_id = (resp.body ).nick_id
+    const data = await UserDB.profiles.findOne({nick_id})
+    return new Response(JSON.stringify(data),{status:200 , headers:headCore(req.headers)})
+
+  }catch(_e){
+    return new Response(JSON.stringify({message:_e}), {status:404 , headers:headCore(req.headers)})
+  }
 }
+
 
--- API/controller/user/profile/put.ts
+++ API/controller/user/profile/put.ts
@@ -1,5 +1,28 @@
+import { headCore } from "../../../service/headCore/main.ts"
+import { Authenticate } from "../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../db/user/main.ts"
+import { ModelUserProfile } from "../../../model/user/profile.ts"
+
 export async function _put(req:Request){
-  console.log(" this is controller /account => pout")
-  return new Response()
+  console.log(" setting PUT")
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers) }) } 
+  // model 
+  const body= await req.json()
+  const ok_account = await ModelUserProfile.Put( body )
+  if ( ! ok_account.ok ){ return new Response(JSON.stringify({message:ok_account.message}) , { status:404 , headers:headCore(req.headers) }) }
+
+  // put mongo
+  try{
+  const nick_id = (resp.body ).nick_id
+  
+  await UserDB.profiles.updateOne({nick_id},{$set:body})
+  return new Response(null , {status:200 , headers:headCore(req.headers)})
+
+  }catch(_e){
+    return new Response(JSON.stringify({message:_e}), {status:404 , headers:headCore(req.headers)})
+  }
 }
 
