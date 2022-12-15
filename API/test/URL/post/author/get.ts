36484b792e9551229dbdf8689fde61ab716fb256
flynth <flynth.dev@gmail.com>
2022-12-15 12:06:00 -0400

    Test: bassic fetch

---
+++ API/test/URL/post/author/get.ts
@@ -0,0 +1,10 @@
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijp7Im5pY2tfaWQiOjJ9fQ.wtsw-IWT8z8gVC4KsWg0KHc7Sv4GW5ozEj8my9rIOlA"
+
+const resp = await fetch("http://localhost:8000/post/author",{
+  method:"GET",
+  headers:{
+    'jwt':jwt
+  },
+})
+console.log(resp)
+//resp.json().then(x=>console.log(x))
