017493d5c45a191a8f1009a642f09e3a91f305b8
flynth <flynth.dev@gmail.com>
2022-12-15 19:07:10 -0400

    Ready to .../post/nick_id *

--- API/controller/user/post/nick_id/delete.ts
+++ API/controller/user/post/nick_id/delete.ts
@@ -1,4 +1,31 @@
+import { ModelUserPost } from "../../../../model/user/post.ts"
+
+import { _route } from "../../../../utils/routes.ts"
+import { headCore } from "../../../../service/headCore/main.ts"
+import { Authenticate } from "../../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../../db/user/main.ts"
+
 export async function _delete(req:Request){
-  console.log(" this is controller /posts/nick_id => delete")
-  return new Response()
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ 
+    return new Response(JSON.stringify({message:resp.message}) , { status:406 , headers:headCore(req.headers)} )
+  }
+
+  try{
+
+  // mongo post
+  const nick_id =Number.parseInt(  (_route(req.url))[2]  )
+  if( !nick_id ){ return new Response(JSON.stringify({ message:"eeror" }),{status:500,headers:headCore(req.headers)})}
+  await UserDB.posts.deleteOne({nick_id})
+
+  return new Response(null,{status:200,headers:headCore(req.headers)})
+
+
+  }catch(_e){
+    return new Response(null,{status:405,headers:headCore(req.headers)})
+  }
+  
 }
+
