5a16dac41fcf6ee7b240674bbe0a8889c9858222
flynth <flynth.dev@gmail.com>
2022-12-14 20:11:21 -0400

    Ready: http://api/account DELETE

--- API/controller/user/account/delete.ts
+++ API/controller/user/account/delete.ts
@@ -1,4 +1,35 @@
+import { headCore } from "../../../service/headCore/main.ts"
+import { Authenticate } from "../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../db/user/main.ts"
+
+
 export async function _delete(req:Request){
-  console.log(" this is controller /account => delete")
-  return new Response()
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers) }) } 
+  
+  // delete user to db
+  const nick_id = (resp.body ).nick_id
+  try{ 
+    // delete account(account)
+    await UserDB.accounts.deleteOne({ nick_id })
+    // delete profile(profile)
+    await UserDB.profiles.deleteOne({ nick_id })
+    // delete setting(base)
+    await UserDB.setting.deleteOne({ nick_id })
+    // delete savePost(base)
+    await UserDB.setting.deleteOne({ nick_id})
+    // delete notifiacion ( nick_id )
+    await UserDB.notices.deleteMany({nick_id})
+    // delete post     ( autho:nick_id )
+    await UserDB.posts.deleteMany({author:nick_id})
+    // delete comment   ( author: nick_id )
+    await UserDB.comments.deleteMany({author:nick_id})
+    // delete comment  ( nick_id )
+    await UserDB.comments.deleteMany({ nick_id })
+    return new Response(null , {status:200 , headers:headCore(req.headers)})
+  }catch(_e){
+    return new Response(JSON.stringify({message:_e}), {status:404 , headers:headCore(req.headers)})
+  }
 }
