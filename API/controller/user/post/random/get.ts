c48932c4b49e2d4fb50292b6ca9758139d022a07
flynth <flynth.dev@gmail.com>
2022-12-15 19:10:13 -0400

    Ready to ../post/random GET

--- API/controller/user/post/random/get.ts
+++ API/controller/user/post/random/get.ts
@@ -1,4 +1,23 @@
+import { headCore } from "../../../../service/headCore/main.ts"
+import { Authenticate } from "../../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../../db/user/main.ts"
+
 export async function _get(req:Request){
-  console.log(" this is controller /posts/random => get")
-  return new Response()
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ 
+    return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers)} )
+  }
+
+  try{
+  
+  const data = await UserDB.posts.find({}).toArray()
+
+  return new Response(JSON.stringify(data),{status:200,headers:headCore(req.headers)})
+  }catch(_e){
+
+    return new Response(JSON.stringify({ message:"eeror" }), {status:500 , headers:headCore(req.headers)})
+  }
 }
+
