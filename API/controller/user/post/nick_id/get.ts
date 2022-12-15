9c160268bce1011b1a435153c13e355a31bc07d8
flynth <flynth.dev@gmail.com>
2022-12-15 18:14:16 -0400

    Ready ...post/nick_id GET

--- API/controller/user/post/nick_id/get.ts
+++ API/controller/user/post/nick_id/get.ts
@@ -1,5 +1,27 @@
+import { _route } from "../../../../utils/routes.ts"
+import { headCore } from "../../../../service/headCore/main.ts"
+import { Authenticate } from "../../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../../db/user/main.ts"
+
 export async function _get(req:Request){
-  console.log(" this is controller /posts/nick _id=> get")
-  return new Response()
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ 
+    return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers)} )
+  }
+
+  try{
+  const nick_id =Number.parseInt(  (_route(req.url))[2]  )
+  if( !nick_id  ){ console.log( "nanana" ) }
+  
+  const data = await UserDB.posts.findOne({nick_id})
+  if( !data ){ console.log("undefinde") }
+
+  return new Response(JSON.stringify(data),{status:200,headers:headCore(req.headers)})
+  }catch(_e){
+
+    return new Response(JSON.stringify({ message:"eeror" }), {status:500 , headers:headCore(req.headers)})
+  }
 }
 
