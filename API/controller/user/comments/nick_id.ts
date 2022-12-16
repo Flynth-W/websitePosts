0b4de6b5310a53bef4efa3b8efa49b74b7944df2
flynth <flynth.dev@gmail.com>
2022-12-16 15:05:29 -0400

    Include: nick_id to comments

---
+++ API/controller/user/comments/nick_id.ts
@@ -0,0 +1,9 @@
+import { UserDB  } from "../../../db/user/main.ts"
+
+export async function _nick_id():Promise<number>{
+  const resp = await UserDB.comments.find().toArray()
+  const leng = resp.length 
+  const lastNick= resp[leng -1]
+  if( lastNick === undefined ){ return 0 }
+  return lastNick.nick_id + 1
+}
