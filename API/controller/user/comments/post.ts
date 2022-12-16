4cfdbf8eed636df84ef20f0a63dd2baf90715b6c
flynth <flynth.dev@gmail.com>
2022-12-16 14:38:15 -0400

    Ready ../comments POST

--- API/controller/user/comments/post.ts
+++ API/controller/user/comments/post.ts
@@ -1,5 +1,36 @@
+import { ModelUserComment } from "../../../model/user/comment.ts"
+import type { TypeUserComment } from "../../../model/user/comment.ts"
+
+import { headCore } from "../../../service/headCore/main.ts"
+import { Authenticate } from "../../../middleware/Authentication.ts"
+import { UserDB }  from "../../../db/user/main.ts"
+
 export async function _post(req:Request){
-  console.log(" this is controller /comment => post")
-  return new Response()
+  // authneticate jwt
+  const jwt = req.headers.get("jwt") || ""
+  const resp = await Authenticate(jwt)
+  if(!resp.ok){ 
+    return new Response(JSON.stringify({message:resp.message}) , { status:404 , headers:headCore(req.headers)} )
+  }
+
+  try{
+  // model post
+  const body= await req.json()
+  const _model = await ModelUserComment.Post( body )
+  if ( !_model.ok ){ 
+    return new Response(JSON.stringify({message:_model.message}) , { status:404 , headers:headCore(req.headers) }) 
+  }
+
+  // mongo post
+  const nick_id = (resp.body ).nick_id
+  const comment:TypeUserComment={author:nick_id,..._model.body}
+  console.log(comment)
+  await UserDB.comments.insertOne(comment)
+  return new Response(null , {status:200 , headers:headCore(req.headers)})
+
+
+  }catch(_e){
+    return new Response(null,{status:404,headers:headCore(req.headers)})
+  }
 }
 
