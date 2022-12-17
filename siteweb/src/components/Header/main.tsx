a4b65ccda8553682ba7a4a3192a30e1020d7f54e
flynth <flynth.dev@gmail.com>
2022-12-16 23:50:33 -0400

    Ready base  component/Top

--- siteweb/src/components/Header/main.tsx
+++ siteweb/src/components/Header/main.tsx
@@ -1,5 +1,12 @@
+import {User} from "./user/main"
+import { Navigation } from "./navigation/main"
+import { Search } from "./search/main"
 export function Header (){
   return (
-    <div>Top Page</div>
+    <div className="Top">
+      <User/>
+      <Navigation/>
+      <Search/>
+    </div>
   )
 }
