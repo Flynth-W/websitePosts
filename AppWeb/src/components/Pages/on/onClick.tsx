429261822e1dae54ebdc62fc7e4e64d8627b023a
flynth <flynth.dev@gmail.com>
2023-01-05 14:35:03 -0400

    Init Onclick to Pages

--- AppWeb/src/components/Pages/on/onClick.tsx
+++ AppWeb/src/components/Pages/on/onClick.tsx
@@ -1,8 +1,8 @@
 export function _onclick(id:string,x:any,cmpts:any){
   //console.log(x.target.attributes)
   //if (x.target.attributes.id.nodeValue === id || x.target.attributes.id.nodeValue === "any"){
-
     if( !!cmpts.notify.show.get ){ 
+      console.log(x)
       cmpts.notify.data.set(<></>)
       cmpts.notify.show.set(false)
     }
