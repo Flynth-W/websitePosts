edf958f379cd5bebdf071bbdba91690fe427734d
flynth <flynth.dev@gmail.com>
2023-01-06 21:25:39 -0400

    Include avatar svg

--- AppWeb/src/components/Header/user/avatar.tsx
+++ AppWeb/src/components/Header/user/avatar.tsx
@@ -1,19 +1,22 @@
 import  Image  from "next/image"
 import React, { useState  } from 'react';
 
+//      <Image id="imageUser" 
+//        src="/favicon.jpg" 
+//        alt="my picture"
+//        height={60}
+//        width={60}
+//        style={{
+//            borderRadius: "50%", //👈 and here you can select border radius
+//          }}
+//      />
+import { SVGAvatar } from "./avatar.svg";
+
 export function Avatar (){
 
   return (
     <div className="HUAvatar" >
-      <Image id="imageUser" 
-        src="/favicon.jpg" 
-        alt="my picture"
-        height={60}
-        width={60}
-        style={{
-            borderRadius: "50%", //👈 and here you can select border radius
-          }}
-      />
+      <SVGAvatar/>
     </div>
   )
 }
